4bc380fc202a7d7a4173ae101fd3d353cae0dfd1
duxard <duxard@gmail.com>
2021-01-12 11:03:15 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,15 @@ export class AppComponent {
   title = 'ng-grid-practice';
 
   columnDefs = [
-    { field: 'make', sortable: true, filter: true  },
+    {
+      field: 'make',
+      sortable: true,
+      filter: true,
+      sortingOrder: ['desc', 'asc', null],
+      minWidth: 500,
+      cellStyle: {color: 'red', 'background-color': 'yellow'}
+    },
+
     { field: 'model' },
     { field: 'price'}
   ];
@@ -17,6 +25,15 @@ export class AppComponent {
   rowData = [
     { make: 'Toyota', model: 'Celica', price: 35000 },
     { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxter', price: 72000 }
+    { make: 'Aorsche', model: 'Boxter2', price: 72000 },
+    { make: 'Borsche', model: 'Boxter3', price: 72000 },
+    { make: 'Zorsche', model: 'Boxter4', price: 72000 }
   ];
+
+  onGridReady(event) {
+    console.warn('Grid ready!');
+    console.dir(event);
+  }
+
+
 }
